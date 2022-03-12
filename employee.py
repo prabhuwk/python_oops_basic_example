67957a0ed0f86dfd44866a54d1b0c1bc7fa2af45
Prabhu K <prabhu.k@icloud.com>
2022-03-12 22:02:01 +0000

    Create account and staff report

--- employee.py
+++ employee.py
@@ -1,6 +1,7 @@
 class Employee:
-    def __init__(self, name: str, salary: int):
-        self.name = name
+    def __init__(self, first_name: str, last_name: str, salary: int):
+        self.first_name = first_name
+        self.last_name = last_name
         self.salary = salary
 
 class Manager(Employee):
--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from employee import Manager, Developer, Tester
 
 employees = [
-    Manager("Jack", 4000),
-    Developer("Jill", 3000),
-    Developer("Chris", 3000),
-    Tester("Dane", 2500),
+    Manager("Jack", "Brown", 4000),
+    Developer("Jill", "Jones", 3000),
+    Developer("Chris", "Dawn", 3000),
+    Tester("Dane", "Joey", 2500),
 ]
 
-for e in employees:
-    print(f"{e.name}, ${e.salary}, {e.job_title}")
+def print_account_report():
+    print("Account Report")
+    print("==============")
+    for e in employees:
+        print(f"{e.first_name} {e.last_name}, ${e.salary}")
+
+def print_staff_report():
+    print("Staff Report")
+    print("============")
+    for e in employees:
+        print(f"{e.first_name} {e.last_name}, {e.job_title}")
+
+print_account_report()
+print()
+print_staff_report()
